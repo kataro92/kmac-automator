714caa022319cbc230fd674a857daf8587c2c35c
Phạm Huy Đức <ducph13@fsoft.com.vn>
2025-12-25 06:38:42 +0700

    Initial Commit

---
+++ KMacAutomator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KMacAutomator
+//
+//  Created by MysteryM1 on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KMacAutomator/KMacAutomatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMacAutomatorApp.swift
+//  KMacAutomator
+//
+//  Created by MysteryM1 on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct KMacAutomatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
